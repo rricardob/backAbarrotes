69f89f5212c1f1de344f5511ea5aa49d0454d1b9
SharonHL <sharon.herrera.luciano@gmail.com>
2022-04-11 23:26:53 -0500

    Servicio para Productos finalizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@ module.exports = app => {
 
   const routerCategoria = require('./routesCategoria');
   const routerCliente = require('./routesCliente');
+  const routerProducto = require('./routesProducto');
 
   app.use('/categoria', routerCategoria);
   app.use('/cliente', routerCliente);
-
+  app.use('/producto', routerProducto);
 };
